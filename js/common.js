25c2060a00b9da996d9aa924c3ec5eb8d7fd45ca
akella <akella.a@gmail.com>
2014-07-24 02:52:38 +0400

    static

--- js/common.js
+++ js/common.js
@@ -1,8 +1,9 @@
 head.ready(function() {
+	 $(".l-col2").sticky({topSpacing:0});
 
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
 
-	console.log($('body').html());
+	//	console.log($('body').html());
 });
--- js/init.js
+++ js/init.js
@@ -1,3 +1,4 @@
 head.load("js/lib/jquery.js", 
+					"js/lib/sticky.js",
 					"js/lib/cycle.js",
 					"js/common.js");
